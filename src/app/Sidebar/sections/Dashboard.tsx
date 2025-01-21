16fd03f65940ff7f174fea3b8e94b90e3eb5350f
dewmindi <udewmindi.ud@gmail.com>
2025-01-22 03:43:49 +0530

    created sidebar

---
+++ src/app/Sidebar/sections/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardSection = () => {
+  return (
+    <div className='bg-yellow-300 h-screen'>
+
+    </div>
+  )
+}
+
+export default DashboardSection
---
+++ src/app/Sidebar/sections/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarSection = () => {
+  return (
+    <div className='bg-slate-100 h-screen'>
+        
+    </div>
+  )
+}
+
+export default SidebarSection
